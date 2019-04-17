c3deedb0244052a61749a9d323c2f466174a633e
이호준 <monkey@monkey.local>
2019-04-17 18:23:16 +0900

    loading btn

--- myApp/public/javascripts/main.js
+++ myApp/public/javascripts/main.js
@@ -11,37 +11,35 @@ function deploy(){
 	return false;
 };
 
-/*
-function checkGoal(addr){
-    $.ajax({
-		url: "/checkGoal",
-        type: "post",
-		data: {address: addr},
-        success: function(result){
-            $("#checkGoal").text(result);
-        }
-    });
 
-    return false;
-}
-*/
+var loading=$("#loader");
 $(document).ready(function(){
+	loading.show();
 	$("#checkGoalBtn").click(function(){
+
 		$.ajax({
         	url: "/checkGoal",
         	type: "post",
         	data: {address: '0x30070a189bd36ddc3b511d15e2ad656dd2fbb6d3'},
         	success: function(result){
-        	    $("#checkGoal").text(result);
+				$("#checkGoal").text(result);
+				loading.hide();
         	}
     	});
-	
-	var btn = $(this);
-    btn.prop('disabled',true);
+
+		var btn = $(this);
+    	btn.prop('disabled',true);
     	window.setTimeout(function(){ 
     	    btn.prop('disabled',false);
-    },6000);
-	return false;
+    	},6000);
+		return false;
 	});
+
+	$( "#checkGoalBtn" ).click(function() {
+		loader();
+	});
+	function loader(div){
+		$("#loader").toggle();
+	}
 });
 
